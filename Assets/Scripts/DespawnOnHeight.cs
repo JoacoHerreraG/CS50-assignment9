7e447ce9a4ddca813cfa5c52fa2a0c21ce914d4b
Joaquin Herrera Gayol <joaco_hg@hotmail.com>
2021-03-29 18:24:22 -0300

    uploading data

---
+++ Assets/Scripts/DespawnOnHeight.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DespawnOnHeight : MonoBehaviour {
+
+	public GameObject characterController;
+	public static AudioSource whisperSound;
+
+	// Use this for initialization
+	void Start () {
+		whisperSound = DontDestroy.instance.GetComponents<AudioSource>()[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (characterController.transform.position.y < -1) {
+			whisperSound.Stop();
+			FloorCount.floor = 1;
+			SceneManager.LoadScene("GameOver");
+		}
+	}
+}
